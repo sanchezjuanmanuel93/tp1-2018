3001803a9407b7881f9ec72a0f9ac2ee310a1a5b
Juan Manuel Sanchez <sanchez.juanmy@gmail.com>
2018-09-01 17:37:37 -0300

    JS - style matches

--- src/js/common/Match/Match.js
+++ src/js/common/Match/Match.js
@@ -20,21 +20,24 @@ class Match extends React.Component {
         } = this.props.match;
 
         return (
-            <Link to={`/match/${num}`}>
-                <div className="match" key={num}>
-                    <div className="match-date">
-                        <div>{date} {time}</div>
-                        <div>{stadium.name}</div>
-                        <div>{city}</div>
+            <div className="match-container" key={num}>
+                <Link to={`/match/${num}`}>
+                    <div className="match">
+                        <div className="match-date">
+                            <div>{date} {time}</div>
+                            <div>{stadium.name}</div>
+                            <div>{city}</div>
+                        </div>
+                        <div className="match-score">
+                            <span>{team1.name}</span>
+                            <img src={`https://api.fifa.com/api/v1/picture/flags-fwc2018-2/${team1.code}`} alt={team1.name} />
+                            <b>{score1} - {score2}</b>
+                            <img src={`https://api.fifa.com/api/v1/picture/flags-fwc2018-2/${team2.code}`} alt={team2.name} />
+                            <span>{team2.name}</span>
+                        </div>
                     </div>
-                    <div className="match-score">
-                        <span>{team1.name}</span><img src={`https://api.fifa.com/api/v1/picture/flags-fwc2018-2/${team1.code}`} alt={team1.name} />
-                        <b>{score1} - {score2}</b>
-                        <img src={`https://api.fifa.com/api/v1/picture/flags-fwc2018-2/${team2.code}`} alt={team2.name} />
-                        <span>{team2.name}</span>
-                    </div>
-                </div>
-            </Link>
+                </Link>
+            </div >
         );
     }
 }
